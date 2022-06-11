70a36c3d5f22b0bfcce1b7c5988bc3a777f64ddb
Julius <johncrist2000@gmail.com>
2022-06-11 20:30:03 +1200

    feat/Member detail

---
+++ client/src/app/_interceptors/jwt.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, take } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private accountService: AccountService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    let currentUser: User | undefined;
+
+    this.accountService.currentUser$
+      .pipe(take(1))
+      .subscribe({ next: (user) => (currentUser = user) });
+
+    if (currentUser) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
